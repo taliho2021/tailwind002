ef66237814c5834f0f32472083cb9801e63b9db5
taliho2021 <tal@analink.com>
2023-02-15 16:42:45 -0500

    Tailwind CSS styling

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {},
+    container: {
+      padding: "2rem",
+      center: true,
+    },
+  },
+  plugins: [],
+};
